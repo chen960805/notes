8f7376ecfade7e55a16b8aa7969a974930ab4299
陈斯炜 <74707462+chen960805@users.noreply.github.com>
2022-07-29 14:27:45 +0800

    小修改

--- regexp/index.js
+++ regexp/index.js
@@ -1,2 +1,2 @@
 // 千分位分隔符正则  '123456789.123456'.replace(thousandSeparated,'$1,') => '123,456,789.123456'
-const thousandSeparated = /(?<=^\d*)(\d)(?=(?:\d{3})+(\.\d*)?$)/g
+const thousandSeparated = /(?<=^\-?\d*)(\d)(?=(?:\d{3})+(\.\d*)?$)/g
